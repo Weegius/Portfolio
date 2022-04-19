a87d0295fb5333186f0fc72841902b2f1c2569d8
Christian Thompson <ChristianThompson1@hotmail.com>
2022-04-18 19:05:17 -0700

    testing formatting changes

--- src/components/home.js
+++ src/components/home.js
@@ -6,14 +6,15 @@ const Home = () => {
   return (
     <>
       <AnimatePresence>
+        
         <Flex
-          p={3}
+          // p={3}
           justifyContent={"space-between"}
-          flexDir={"column"}
           h="100%"
           w="100%"
           borderColor={"white"}
           borderWidth={1}
+          bg={"blue"}
         >
           <motion.div
             initial={{ opacity: 0 }}
@@ -21,16 +22,21 @@ const Home = () => {
             exit={{ opacity: 0 }}
           >
             <Flex alignSelf={"center"} flexDir="column">
+
+              {/* ----------------- NAME -------------------- */}
               <Flex
                 display={["flex", "flex", "none", "none"]}
                 alignSelf="center"
                 flexDir={"column"}
                 fontSize={[70, 100, 20, 20, 1]}
+                bg="white"
               >
                 <Text>Christian</Text>
                 <Text mt={-10}>Thompson</Text>
               </Flex>
+
               <Flex alignSelf={"center"}>
+                {/* ------------- WHITE BOX AROUND IMAGE -------------------- */}
                 <Flex
                   pos="absolute"
                   alignSelf={"center"}
@@ -42,7 +48,7 @@ const Home = () => {
                   h={[230, 250, 350, 400]}
                   display={["flex", "none", "flex", "flex"]}
                 />
-                {/*------Images------- */}
+                {/*--------------------- IMAGE -------------------- */}
                 <Image
                   src="./images/headshot.jpg"
                   alt="Headshot"
@@ -54,17 +60,21 @@ const Home = () => {
                 />
               </Flex>
 
-              <Flex
-                flexDir={["none", "column", "none", "column"]}
-                fontSize={[30, 60, 60, 60]}
-                mt={[5, 10, 10, 0]}
-              >
-                <Text>Front End</Text>
-                <Text ml={10} mt={-15}>
-                  Web Developer
-                </Text>
-              </Flex>
             </Flex>
+
+              {/* --------------- BOTTOM TEXT ---------------- */}
+
+            <Flex
+              // flexDir={["row", "row", "none", "row"]}
+              fontSize={[30, 60, 60, 60]}
+              mt={[5, 10, 10, 0]}
+              // flexDir={"row"}
+              bg="yellow"
+            >
+              <Text>Front End</Text>
+              <Text>Web Developer</Text>
+            </Flex>
+
           </motion.div>
         </Flex>
       </AnimatePresence>
